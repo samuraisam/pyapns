bf5f21e8ecb40d051895435c69e2e964358c5024
Samuel Sutch <sam@altertap.com>
2010-09-01 11:16:32 -0700

    compact output from dumps in server #4

--- pyapns/server.py
+++ pyapns/server.py
@@ -325,7 +325,7 @@ def encode_notifications(tokens, notifications):
   if type(notifications) is dict and type(tokens) in (str, unicode):
     tokens, notifications = ([tokens], [notifications])
   if type(notifications) is list and type(tokens) is list:
-    return ''.join(map(lambda y: structify(*y), ((binaryify(t), json.dumps(p))
+    return ''.join(map(lambda y: structify(*y), ((binaryify(t), json.dumps(p, separators=(',',':')))
                                     for t, p in zip(tokens, notifications))))
 
 def decode_feedback(binary_tuples):
