f4c20518cb906061dcb5a6a6c1100b7e08f6ccd6
John Watson <john@photodojo.org>
2013-09-05 11:03:29 -0400

    Fix crash when sending UTF-8 characters.
    The `ensure_ascii` kwarg in `json.dumps()` must be `True` (the
    default) so that the subsequent UTF-8 encoding works correctly.

--- pyapns/model.py
+++ pyapns/model.py
@@ -232,8 +232,8 @@ class Notification(object):
                     'token "{}" could not be decoded: {}'.format(str(t), str(e)
                 ))
 
-        encoded_payload = json.dumps(self.payload, separators=(',', ':'),
-                                     ensure_ascii=False).encode('utf-8')
+        encoded_payload = json.dumps(self.payload,
+                                     separators=(',', ':')).encode('utf-8')
         return structify(binaryify(self.token), self.internal_identifier, 
                          self.expiry, encoded_payload)
 
